1f13c791708565722e073c111e0938f6e001bbd9
Khawaja21 <93649410+Khawaja21@users.noreply.github.com>
2023-12-15 14:27:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,49 @@
-// API
-// CRUD 
-// get - read (get the data from the server)
-// post - create (To create a new data)
-// put  - update ()
-// delete - delete
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
-
-// Set up Content Security Policy middleware
-// app.use((req, res, next) => {
-//     res.setHeader(
-//         'Content-Security-Policy',
-//         "script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' https://js.stripe.com; object-src 'self'"
-//     );
-//     next();
-// });
-
-// res.setHeader('Content-Security-Policy', "script-src 'self' https://js.stripe.com");
-// res.setHeader('Content-Security-Policy', "script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules'; script-src-elem https://js.stripe.com; object-src 'self'");
-
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+// Sample data
+let students = [
+];
 
-let students = [];
-
-// API to get the list of students
+// Routes
 app.get('/students', (req, res) => {
-    res.json(students);
+  res.json(students);
 });
+app.get('/students/:id', (req, res) => {
+    // res.json(students);
+  });
+  
 
-// API to add a new student(post method use kartai huai)
+
+
+
 app.post('/students', (req, res) => {
-    const newStudent = req.body;
-    students.push(newStudent);
-    res.json({ message: 'Student added successfully' });
+  const newStudent = req.body;
+  newStudent.id = students.length + 1;
+  students.push(newStudent);
+  res.json(newStudent);
 });
 
-// API to update a student(put method use kartai huai)
 app.put('/students/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedStudent = req.body;
-    students[id] = updatedStudent;
-    res.json({ message: 'Student updated successfully' });
+  const studentId = parseInt(req.params.id);
+  const updatedStudent = req.body;
+  students = students.map(student =>
+    student.id === studentId ? { ...student, ...updatedStudent } : student
+  );
+  res.json(updatedStudent);
 });
 
-// API to delete a student(delete method use kartai huai)
 app.delete('/students/:id', (req, res) => {
-    const id = req.params.id;
-    students.splice(id, 1);
-    res.json({ message: 'Student deleted successfully' });
+  const studentId = parseInt(req.params.id);
+  students = students.filter(student => student.id !== studentId);
+  res.json({ message: 'Student deleted successfully' });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at localhost${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
