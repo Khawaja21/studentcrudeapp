572ac490db544901296d9aed1011634328d35e0e
Khawaja21 <93649410+Khawaja21@users.noreply.github.com>
2023-12-15 14:22:42 +0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,119 +1,111 @@
-document.addEventListener('DOMContentLoaded', function() {
-    loadStudents();
-});
-
-function loadStudents() {
-    fetch('/students')
+document.addEventListener('DOMContentLoaded', function () {
+    const studentTableBody = document.getElementById('studentTableBody');
+    const studentForm = document.getElementById('studentForm');
+  
+    // Fetch students from the backend and update the table
+    function updateStudentTable() {
+      fetch('/students')
+        .then(response => response.json())
+        .then(students => {
+          studentTableBody.innerHTML = ''; // Clear the existing rows
+  
+          students.forEach(student => {
+            const row = studentTableBody.insertRow();
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            const cell3 = row.insertCell(2);
+            const cell4 = row.insertCell(3);
+  
+            cell1.innerHTML = student.id;
+            cell2.innerHTML = student.name;
+            cell3.innerHTML = student.age;
+  
+            // Add buttons for edit and delete
+            const editButton = document.createElement('button');
+            editButton.textContent = 'Edit';
+            editButton.addEventListener('click', () => editStudent(student.id));
+            cell4.appendChild(editButton);
+  
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', () => deleteStudent(student.id));
+            cell4.appendChild(deleteButton);
+          });
+        });
+    }
+  
+    // Function to handle form submission (add/update student)
+    function handleFormSubmit(event) {
+      event.preventDefault();
+  
+      const nameInput = document.getElementById('name');
+      const ageInput = document.getElementById('age');
+  
+      const formData = {
+        name: nameInput.value,
+        age: ageInput.value,
+      };
+  
+      const isEditMode = !!editingStudentId;
+  
+      const url = isEditMode ? `/students/${editingStudentId}` : '/students';
+      const method = isEditMode ? 'PUT' : 'POST';
+  
+      fetch(url, {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
         .then(response => response.json())
         .then(data => {
-            const studentList = document.getElementById('studentList');
-            studentList.innerHTML = '';
-
-            data.forEach((student, index) => {
-                const li = document.createElement('li');
-                li.innerHTML = `${student.name}, ${student.age} years old 
-                    <button onclick="deleteStudent(${index})">Delete</button>
-                    <button onclick="editStudent(${index})">Edit</button>`;
-                studentList.appendChild(li);
-            });
+          console.log(data);
+          updateStudentTable();
+          clearForm();
         });
-}
-
-function addStudent() {
-    const nameInput = document.getElementById('studentName');
-    const ageInput = document.getElementById('studentAge');
-
-    const student = {
-        name: nameInput.value,
-        age: ageInput.value
-    };
-
-    fetch('/students', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(student),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.message);
-        loadStudents();
-        clearForm();
-    });
-}
-
-function deleteStudent(index) {
-    fetch(`/students/${index}`, {
-        method: 'DELETE',
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.message);
-        loadStudents();
-    });
-}
-
-// function editStudent(index) {
-//     fetch(`/students/${index}`, {
-//         method: 'Edit',
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data.message);
-//         loadStudents();
-//     });
-// }
-
-function editStudent(index) {
-    const nameInput = document.getElementById('studentName');
-    const ageInput = document.getElementById('studentAge');
-
-    const studentList = document.getElementById('studentList');
-    const selectedStudent = studentList.children[index].textContent.split(', ');
-
-    nameInput.value = selectedStudent[0];
-    ageInput.value = selectedStudent[1].split(' ')[0];
-
-   // Store the index of the edited student in a hidden field
-    document.getElementById('editIndex').value = index;
-}
-
-function clearForm() {
-    const nameInput = document.getElementById('studentName');
-    const ageInput = document.getElementById('studentAge');
-
-    // Check if the elements exist before trying to access them
-    if (nameInput && ageInput) {
-        nameInput.value = '';
-        ageInput.value = '';
-    } else {
-        console.error('One or both form elements are null or not found.');
     }
-}
+  
+    // Function to edit a student
+    function editStudent(studentId) {
+      // Fetch student details and populate the form for editing
+      fetch(`/students/${studentId}`)
+        .then(response => response.json())
+        .then(student => {
+          document.getElementById('name').value = student.name;
+          document.getElementById('age').value = student.age;
+          editingStudentId = studentId;
+        });
+    }
 
-//Function to update an edited student
-function updateStudent() {
-    const nameInput = document.getElementById('studentName');
-    const ageInput = document.getElementById('studentAge');
-    const editIndex = document.getElementById('editIndex').value;
 
-    const updatedStudent = {
-        name: nameInput.value,
-        age: ageInput.value
-    };
-
-    fetch(`/students/${editIndex}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedStudent),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.message);
-        loadStudents();
-        clearForm();
-    });
-}
+  
+    // Function to delete a student
+    function deleteStudent(studentId) {
+      fetch(`/students/${studentId}`, {
+        method: 'DELETE',
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.message);
+          updateStudentTable();
+        });
+    }
+  
+    // Function to clear the form
+    function clearForm() {
+      document.getElementById('name').value = '';
+      document.getElementById('age').value = '';
+      editingStudentId = null;
+    }
+  
+    // Initialize editingStudentId
+    let editingStudentId = null;
+  
+    // Attach event listeners
+    studentForm.addEventListener('submit', handleFormSubmit);
+  
+    // Initial data fetch and table update
+    updateStudentTable();
+  });
+  
